ce171a4c40a3ef764cadf5b5ac3382fea2cb31e9
Brendan McVeigh <mcveigh.brendan@gmail.com>
2016-12-29 17:19:47 -0800

    Add function to similate comparison matrix based on probability vectors and MatchMatrix

--- src/simrecords.jl
+++ src/simrecords.jl
@@ -1,6 +1,21 @@
 import StatsBase
 include("matching_matrix.jl")
 
+"""
+Simulate data corresponding to binary comparison metrics for record linkage, enforcing single linkage of records
+
+Requires with a total number of records as well as the number of records sampled in each of the two populations compared
+along with M and U probability vectors.  In this case an underlying MatchMatrix will be returned along with the simulated
+comparison values.  Comparisons are returned in the form of a 3-dimensional Array of 0s and 1s
+
+Alternatively a MatchMatrix can be supplied along with M and U probability vectors.  In this case only the simulated data
+will be returned.  
+
+* Examples
+simulate_singlelinkage_binary(1000, 300, 200, [0.8, 0.82, 0.95, 0.98], [0.2, 0.3, 0.38, 0.41])
+C = MatchMatrix(StatsBase.sample(1:300, 15, replace = false), StatsBase.sample(1:200, 15, replace = false), 300, 200)
+simulate_singlelinkage_binary(C, [0.8, 0.82, 0.95, 0.98], [0.2, 0.3, 0.38, 0.41])
+"""
 function simulate_singlelinkage_binary{G <: AbstractFloat, T <: Integer}(nrecords::T, na::T, nb::T, pM::Array{G, 1}, pU::Array{G, 1})
     if na > nrecords
         error("Number of records must be a least as large as number of records in group A")
@@ -34,4 +49,27 @@ function simulate_singlelinkage_binary{G <: AbstractFloat, T <: Integer}(nrecord
         end
     end
     return out, trueC
+end
+
+function simulate_singlelinkage_binary{G <: AbstractFloat, T <: Integer}(C::MatchMatrix, pM::Array{G, 1}, pU::Array{G, 1})
+    if any(pM .< 0.0) || any(pM .> 1.0)
+        error("M probabilities must be between 0 and 1")
+    end
+    if any(pU .< 0.0) || any(pU .> 1.0)
+        error("U probabilities must be between 0 and 1")
+    end
+    if length(pM) != length(pU)
+        error("Length of M probabilities and U probabilities must match")
+    end
+    nmeasure = length(pM)
+    out = Array{Int64}(nmeasure, C.nrow, C.ncol)
+    for ii in C.nrow
+        for jj in C.ncol
+            out[:, ii, jj] = Int64.(rand(nmeasure) .< pU)
+        end
+    end
+    for kk in 1:length(C.rows)
+        out[:, C.rows[kk], C.cols[kk]] = Int64.(rand(nmeasure) .< pM)
+    end
+    return out
 end
