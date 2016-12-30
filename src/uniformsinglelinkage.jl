ccee19516ffdc72873a8e0782ae5cf00e19a3df8
Brendan McVeigh <mcveigh.brendan@gmail.com>
2016-12-30 15:33:52 -0800

    define uniform distribution of single linkage match matricies given number of matches and matrix dimension

---
+++ src/uniformsinglelinkage.jl
@@ -0,0 +1,79 @@
+"""
+Uniform distribution over single linkage matching matricies
+"""
+import StatsBase
+import Distributions
+include("matching_matrix.jl")
+
+
+immutable UniformSingleLinkage{G <: Integer} <: Distributions.DiscreteMatrixDistribution
+    nrow::G # Number of rows
+    ncol::G # Number of columns
+    t::G # Number of Matches
+
+    #function UniformSingleLinkage(nrow::G, ncol::nc, t::G)
+    #    @check_args(UniformSingleLinkage, zero(t) <= t <= max(nrow, ncol))
+    #    @check_args(UniformSingleLinkage, zero(nrow) <= nrow)
+    #    @check_args(UniformSingleLinkage, zero(ncol) <= ncol)
+    #end
+end
+
+#### Outer constructors
+UniformSingleLinkage{G <: Integer}(nrow::G, ncol::G, t::G) = UniformSingleLinkage{G}(nrow, ncol, t)
+UniformSingleLinkage(nrow::Real, ncol::Real, t::Real) = UniformSingleLinkage(promote(nrow, ncol, t)...)
+UniformSingleLinkage(d::Integer, t::Integer) = UniformSingleLinkage(d, d, t)
+
+#### Parameters
+
+params(d::UniformSingleLinkage) = (d.nrow, d.ncol, d.t)
+@inline partype{G<:Integer}(d::UniformSingleLinkage{G}) = G
+
+#### Evaluation
+
+function Distributions.pdf(d::UniformSingleLinkage, M::MatchMatrix)
+    if d.t != length(M.rows) || d.nrow != M.nrow || d.ncol != 
+        return 0.0
+    elseif d.t == 0
+        return 1.0
+    else
+        return (factorial(d.t) * binomial(M.nrow, d.t) * binomial(M.ncol, d.t)) ^ -1.
+    end
+end
+
+function Distributions.logpdf(d::UniformSingleLinkage, M::MatchMatrix)
+    if d.t != length(M.rows)
+        return -Inf
+    elseif d.t ==  0
+        return 0.0
+    elseif d.t == M.nrow && d.t == M.ncol
+        return -sum(log(1:t))
+    elseif d.t == M.nrow && d.t != M.ncol
+        return -sum(log((M.ncol - d.t + 1):M.ncol))
+    elseif d.t != M.nrow && d.t == M.ncol
+        return -sum(log((M.nrow - d.t + 1):M.nrow))
+    else
+        return -sum(log((M.nrow - d.t + 1):M.nrow)) - sum(log((M.ncol - d.t + 1):M.ncol)) + sum(log(1:d.t))
+    end
+end
+
+#### Sampling
+
+function rand(d::UniformSingleLinkage)
+    rows = StatsBase.sample(1:d.nrow, d.t)
+    cols = StatsBase.sample(1:d.ncol, d.t)
+    return MatchMatrix(rows, cols, d.nrow, d.ncol)
+end
+
+
+#### Test things run, move to unit tests
+d = UniformSingleLinkage(5, 5, 3)
+Distributions.pdf(d, MatchMatrix([1], [1], 5, 5))
+
+Distributions.pdf(d, MatchMatrix([1, 2, 3], [1, 2, 3], 5, 5))
+(factorial(3) * binomial(5, 3) * binomial(5, 3)) ^ -1.
+
+log(Distributions.pdf(d, MatchMatrix([1, 2, 3], [1, 2, 3], 5, 5)))
+Distributions.logpdf(d, MatchMatrix([1, 2, 3], [1, 2, 3], 5, 5))
+
+params(d)
+rand(d)
