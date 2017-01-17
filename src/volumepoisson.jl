d10d341b9e1355569bda26152a4803cb9ef2462e
Brendan McVeigh <mcveigh.brendan@gmail.com>
2017-01-17 09:14:49 -0800

    add function for computing ratio between density at L and L + 1

--- src/volumepoisson.jl
+++ src/volumepoisson.jl
@@ -25,14 +25,23 @@ function VolumePoisson{T <: Real, G <: Integer}(θ::T, n::G, m::G)
         diff = upper - lower
         inc = n * θ + sum(log(1:n)) + sum(log(1:diff))
         tot = exp(-inc)
-        for ii in 1:n
+        for ii in 1:lower
             inc += -θ - log(n + 1 - ii) + log(ii) + log(diff + ii)
             tot += exp(-inc)
         end
         return VolumePoisson{T, G}(θ, n, n, 1. / tot)
     end
 end
-    
+
+function coefficients(d::VolumePoisson)
+    maxL = min(d.n,d.m) - 1
+    out = Array{Float64}(maxL + 1)
+    for ii in 0:maxL
+        out[ii+1] = (d.n - ii) * (d.m - ii) / (ii + 1) * exp(-d.θ)
+    end
+    return out
+end
+
 @Distributions.distr_support VolumePoisson 0 (d.θ == zero(typeof(d.θ)) ? 0 : min(d.n, d.m))
 
 #### Parameters
@@ -46,7 +55,7 @@ params(d::VolumePoisson) = (d.θ, d.n, d.m, d.α)
 Distributions.pdf(d::VolumePoisson, x::Int64) = d.α * exp(-x * d.θ - sum(log(1:x)) - sum(log(1:(d.n - x))) - sum(log(1:(d.m - x))))
 Distributions.pdf(d::VolumePoisson, x::Int32) = d.α * exp(-x * d.θ - sum(log(1:x)) - sum(log(1:(d.n - x))) - sum(log(1:(d.m - x))))
 Distributions.logpdf(d::VolumePoisson, x::Int64) = log(d.α) - x * d.θ - sum(log(1:x)) - sum(log(1:(d.n - x))) - sum(log(1:(d.m - x)))
-Distributions.logpdf(d::VolumePoisson, x::Int32) = log(d.α) - x * d.θ - sum(log(1:x)) - sum(log(1:(d.n - x))) - sum(log(1:(d.m - x)))
+Distributions.logpdf(d::VolumePoisson, x::Int32) = log(d.α) - x * d.θ - sum(log(1:x)x) - sum(log(1:(d.n - x))) - sum(log(1:(d.m - x)))
 
 #### Sampling
 
@@ -64,6 +73,10 @@ end
 #### Test things run, move to unit tests
 d1 = VolumePoisson(2, 100, 100)
 d2 = VolumePoisson(1.5, 1, 1)
+d3 = VolumePoisson(2, 1000, 1000)
+coefficients(d3)[1:10]
+Distributions.pdf(d1, 1) / Distributions.pdf(d1, 0)
+
 
 Distributions.rand(d1)
 Distributions.rand(d2)
