cb684b0be901aae0db57464c0df24c16dbfb3e59
Brendan McVeigh <mcveigh.brendan@gmail.com>
2017-01-25 03:55:56 -0500

    add second example with updated plots

--- test/mcmc.jl
+++ test/mcmc.jl
@@ -7,6 +7,7 @@ include("../src/matching_matrix.jl")
 include("../src/logisticnormal.jl")
 include("../src/uniformsinglelinkage.jl")
 include("../src/truncatedpoisson.jl")
+include("../src/volumepoisson.jl")
 include("../src/simrecords.jl")
 include("../src/mcmc.jl")
 
@@ -172,7 +173,6 @@ function transM_ratio{T <: AbstractFloat}(p1::Array{T, 1}, p2::Array{T, 1})
     return exp(logp)
 end
 
-
 #transitionMU::Function #Distributions.rand(d::LogisticNormal)
 function transU{T <: AbstractFloat}(probs::Array{T, 1})
     dArray = LogisticNormal.(probs, 0.8)
@@ -189,50 +189,108 @@ function transU_ratio{T <: AbstractFloat}(p1::Array{T, 1}, p2::Array{T, 1})
     return exp(logp)
 end
 
+#Effect of prior on C...
+function lpC(C::MatchMatrix)
+    return -length(C.rows) * 0.6
+end
+CArray2, MArray2, UArray2, tC, tM, tU = metropolis_hastings_mixing(100000, data, C0, M0, U0, lpC, lpM, lpU, loglikelihood_datatable, transC, transM, transU, transC_ratio, transM_ratio, transU_ratio)
 
-@time CArray2, MArray2, UArray2, tC, tM, tU = metropolis_hastings_mixing(100000, data, C0, M0, U0, lpC, lpM, lpU, loglikelihood_datatable, transC, transM, transU, transC_ratio, transM_ratio, transU_ratio)
+function lpC(C::MatchMatrix)
+    return -length(C.rows) * 1.8
+end
+CArray3, MArray3, UArray3, tC, tM, tU = metropolis_hastings_mixing(100000, data, C0, M0, U0, lpC, lpM, lpU, loglikelihood_datatable, transC, transM, transU, transC_ratio, transM_ratio, transU_ratio)
 
-CArray2
-MArray2
-UArray2
+function lpC(C::MatchMatrix)
+    return -length(C.rows) * 3.0
+end
+CArray4, MArray4, UArray4, tC, tM, tU = metropolis_hastings_mixing(100000, data, C0, M0, U0, lpC, lpM, lpU, loglikelihood_datatable, transC, transM, transU, transC_ratio, transM_ratio, transU_ratio)
 
-sum(tC)
-sum(tM)
-sum(tU)
 
-mean(tC) #.10
-mean(tM) #.061
-mean(tU) #0.016
+#using RCall
+#R"library(ggplot2)"
+#R"library(tidyr)"
+#R"library(latex2exp)"
 
-@rput MArray2 UArray2
+@rput MArray2 UArray2 MArray3 UArray3 MArray4 UArray4
 @rput pM pU
-R"probs2.df <- as.data.frame(cbind(MArray2, UArray2))";
-R"names(probs2.df) <- c('M1', 'M2', 'M3', 'U1', 'U2', 'U3')";
+
+R"probs2.df <- as.data.frame(cbind(1:nrow(MArray2), MArray2, UArray2))";
+R"probs3.df <- as.data.frame(cbind(1:nrow(MArray3), MArray3, UArray3))";
+R"probs4.df <- as.data.frame(cbind(1:nrow(MArray4), MArray4, UArray4))";
+
+R"names(probs2.df) <- c('Iter', 'M1', 'M2', 'M3', 'U1', 'U2', 'U3')";
+R"names(probs3.df) <- c('Iter', 'M1', 'M2', 'M3', 'U1', 'U2', 'U3')";
+R"names(probs4.df) <- c('Iter', 'M1', 'M2', 'M3', 'U1', 'U2', 'U3')";
+
 R"probs2.df <- gather(probs2.df, Comparison, P, M1:U3)";
+R"probs3.df <- gather(probs3.df, Comparison, P, M1:U3)";
+R"probs4.df <- gather(probs4.df, Comparison, P, M1:U3)";
+
 R"genprob.df <- data.frame(Comparison = c('M1', 'M2', 'M3', 'U1', 'U2', 'U3'), genp = c(pM, pU))";
+
+R"pdf('matching_posterior.pdf')"
 R"ggplot(probs2.df, aes(x = P)) + geom_density() + facet_wrap(~Comparison) +
  geom_vline(aes(xintercept = genp), genprob.df, color = 'red') +
- ggtitle('Posterior Matching Probabilities')"
+ ggtitle(TeX('Posterior Matching Probabilities $\\theta = 0.6$'))"
 
+R"ggplot(probs3.df, aes(x = P)) + geom_density() + facet_wrap(~Comparison) +
+ geom_vline(aes(xintercept = genp), genprob.df, color = 'red') +
+ ggtitle(TeX('Posterior Matching Probabilities $\\theta = 1.8$'))"
+
+R"ggplot(probs4.df, aes(x = P)) + geom_density() + facet_wrap(~Comparison) +
+ geom_vline(aes(xintercept = genp), genprob.df, color = 'red') +
+ ggtitle(TeX('Posterior Matching Probabilities $\\theta = 3.0$'))"
+
+R"ggplot(probs2.df, aes(x = Iter, y = P)) + geom_line() + facet_wrap(~Comparison) +
+ geom_hline(aes(yintercept = genp), genprob.df, color = 'red') +
+ ggtitle(TeX('Posterior Matching Probabilities $\\theta = 0.6$'))"
+R"dev.off()"
 
 data1 = gridtoarray(data[1, :, :])
 data2 = gridtoarray(data[2, :, :])
 data3 = gridtoarray(data[3, :, :])
-nmatches, matchdist = totalmatches(CArray2)
-postdens = gridtoarray(matchdist)
+nmatches2, matchdist2 = totalmatches(CArray2)
+postdens2 = gridtoarray(matchdist2)
 
-@rput nmatches data1 data2 data3 postdens
+nmatches3, matchdist3 = totalmatches(CArray3)
+postdens3 = gridtoarray(matchdist3)
+
+nmatches4, matchdist4 = totalmatches(CArray4)
+postdens4 = gridtoarray(matchdist4)
+
+@rput data1 data2 data3 nmatches2 nmatches3 nmatches4 postdens2 postdens3 postdens4
 
 #data processing
-R"matches.df <- data.frame(matches = nmatches, timestep = 1:length(nmatches))";
+R"matches2.df <- data.frame(matches = nmatches2, timestep = 1:length(nmatches2))";
+R"matches3.df <- data.frame(matches = nmatches3, timestep = 1:length(nmatches3))";
+R"matches4.df <- data.frame(matches = nmatches4, timestep = 1:length(nmatches4))";
+
 R"compvec <- unlist(lapply(1:dim(data1)[1], function(n) paste0('(', data1[n, 3], ', ', data2[n, 3], ', ', data3[n, 3], ')')))";
-R"postdens.df <- as.data.frame(postdens)";
-R"names(postdens.df) <- c('row', 'col', 'postmean')";
-R"postdens.df$comparison <- compvec"
+
+R"postdens2.df <- as.data.frame(postdens2)";
+R"postdens3.df <- as.data.frame(postdens3)";
+R"postdens4.df <- as.data.frame(postdens4)";
+
+R"names(postdens2.df) <- c('row', 'col', 'postmean')";
+R"names(postdens3.df) <- c('row', 'col', 'postmean')";
+R"names(postdens4.df) <- c('row', 'col', 'postmean')";
+
+R"postdens2.df$comparison <- compvec"
+R"postdens3.df$comparison <- compvec"
+R"postdens4.df$comparison <- compvec"
 
 #make plots
-R"ggplot(postdens.df) + geom_raster(aes(x = col, y = row, fill = postmean)) +
-geom_text(aes(x = col, y = row, label = comparison), size = 3)"
-
-R"hist(nmatches)"
-R"plot(nmatches)"
+R"pdf('grid_posterior.pdf')"
+R"ggplot(postdens2.df) + geom_raster(aes(x = col, y = row, fill = postmean)) +
+geom_text(aes(x = col, y = row, label = comparison), size = 3) +
+ ggtitle(TeX('Posterior Matching Probabilities $\\theta = 0.6$'))"
+R"ggplot(postdens3.df) + geom_raster(aes(x = col, y = row, fill = postmean)) +
+geom_text(aes(x = col, y = row, label = comparison), size = 3) +
+ ggtitle(TeX('Posterior Matching Probabilities $\\theta = 1.8$'))"
+R"ggplot(postdens4.df) + geom_raster(aes(x = col, y = row, fill = postmean)) +
+geom_text(aes(x = col, y = row, label = comparison), size = 3) +
+ ggtitle(TeX('Posterior Matching Probabilities $\\theta = 3.0$'))"
+R"hist(nmatches2, main = TeX('Posterior Matching Probabilities $\\theta = 0.6$'))"
+R"hist(nmatches3, main = TeX('Posterior Matching Probabilities $\\theta = 1.8$'))"
+R"hist(nmatches4, main = TeX('Posterior Matching Probabilities $\\theta = 3.0$'))"
+R"dev.off()"
