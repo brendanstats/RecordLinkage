c40d0411da8f439a9252d7a1cbba455376517015
Brendan McVeigh <mcveigh.brendan@gmail.com>
2017-02-07 00:16:32 -0500

    wrap code in a module, needs to be tested

---
+++ src/SequentialRecordLinkage.jl
@@ -0,0 +1,61 @@
+module SequentialRecordLinkage
+
+import StatsBase
+import Distributions
+import Base: convert, copy, in
+
+export LogisticNormal,
+    logit,
+    logistic
+
+export TruncatedPoisson
+export VolumePoisson
+export UniformSingleLinkage
+export MatchMatrix,
+    findindex,
+    add_match!,
+    remove_match!,
+    empty_rows,
+    empty_cols,
+    ratio_pmove,
+    match_pairs,
+    totalmatches
+
+export GridMatchMatrix,
+    getgridrow,
+    getgridcol,
+    getrows,
+    getcols,
+    getindicies,
+    getmatches,
+    move_matchgrid,
+    move_matchgrid_exclude
+    
+export simulate_singlelinkage_binary,
+    gridtoarray
+
+export datatotable,
+    loglikelihood_datatable,
+    countones
+
+export UnitKernelDensity,
+    unitkde_slow,
+    unitkde_interpolate,
+    unitkde_tilted
+
+export metropolis_hastings,
+    metropolis_hastings_mixing
+
+include("logisticnormal.jl")
+include("truncatedpoisson.jl")
+include("volumepoisson.jl")
+include("uniformsinglelinkage.jl")
+include("matchmatrix.jl")
+include("gridmatchmatrix.jl")
+include("simrecords.jl")
+include("datatable.jl")
+include("unitkerneldensity.jl")
+include("mcmc.jl")
+include("gridmcmc.jl")
+
+end
