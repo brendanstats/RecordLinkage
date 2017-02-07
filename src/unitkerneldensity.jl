c8fbb6c28c428bac113039be7f0cd26ae4fd1bdc
Brendan McVeigh <mcveigh.brendan@gmail.com>
2017-02-06 19:32:55 -0500

    add method for sampling from UnitKernelDensity and incorporate cdf information in the UnitKernelDensity type

--- src/unitkerneldensity.jl
+++ src/unitkerneldensity.jl
@@ -4,6 +4,7 @@ Define type for storing information about the results of a kernel density estima
 type UnitKernelDensity
     x::Array{Float64, 1}
     y::Array{Float64, 1}
+    cdf::Array{Float64, 1}
     bw::Float64
     npoints::Int64
 end
@@ -13,12 +14,21 @@ Compute a KDE using a normal kernel in range 0 to 1
 """
 function unitkde_slow(data::Array{Float64, 1}, npoints::Int64, bw::Float64)
     d = Distributions.Normal(0.0, bw)
-    x = collect(0.0:(npoints - 1.0)^(-1.0):1.0)
+    stepsize = (npoints - 1.0)^(-1.0)
+    x = collect(0.0:stepsize:1.0)
     y = Array{Float64}(npoints)
+    #Compute point densities
     for (ii, xi) in enumerate(x)
         y[ii] = mean(Distributions.pdf(d, data .- xi))
     end
-    UnitKernelDensity(x, y, bw, npoints)
+    #Compute approximate CDF
+    cdf = zeros(y)
+    cdf[1] = 0.0
+    for ii in 2:npoints
+        cdf[ii] = cdf[ii - 1] + 0.5 * stepsize * (y[ii] + y[ii - 1])
+    end
+    N = cdf[end]^(-1.0)
+    return UnitKernelDensity(x, N .* y, N .* cdf, bw, npoints)
 end
 
 """
@@ -26,11 +36,18 @@ Method to operate on a set of points instead of evenly space points
 """
 function unitkde_slow(data::Array{Float64, 1}, points::Array{Float64, 1}, bw::Float64)
     d = Distributions.Normal(0.0, bw)
-    y = Array{Float64}(length(points))
+    y = zeros(points)
+    #Compute point densities
     for (ii, xi) in enumerate(points)
         y[ii] = mean(Distributions.pdf(d, data .- xi))
     end
-    UnitKernelDensity(points, y, bw, length(points))
+    cdf = zeros(points)
+    #Compute approximate CDF
+    for ii in 2:length(points)
+        cdf[ii] = cdf[ii - 1] + 0.5 * (points[ii] - points[ii - 1]) * (y[ii] + y[ii - 1])
+    end
+    N = cdf[end]^(-1.0)
+    return UnitKernelDensity(points, N .* y, N .* cdf, bw, length(points))
 end
 
 """
@@ -52,12 +69,21 @@ Compute a KDE using a normal kernel in range 0 to 1
 """
 function unitkde_tilted(data::Array{Float64, 1}, npoints::Int64, bw::Float64, θ::Float64)
     d = Distributions.Normal(θ, bw)
-    x = collect(0.0:(npoints - 1.0)^(-1.0):1.0)
+    stepsize = (npoints - 1.0)^(-1.0)
+    x = collect(0.0:stepsize:1.0)
     y = Array{Float64}(npoints)
+    #Compute point densities
     for (ii, xi) in enumerate(x)
         y[ii] = mean(Distributions.pdf(d, data .- xi))
     end
-    UnitKernelDensity(x, y, bw, npoints)
+    #Compute approximate CDF
+    cdf = zeros(y)
+    cdf[1] = 0.0
+    for ii in 2:npoints
+        cdf[ii] = cdf[ii - 1] + 0.5 * stepsize * (y[ii] + y[ii - 1])
+    end
+    N = cdf[end]^(-1.0)
+    return UnitKernelDensity(x, N .* y, N .* cdf, bw, npoints)
 end
 
 """
@@ -65,11 +91,27 @@ Method to operate on a set of points instead of evenly space points
 """
 function unitkde_slow(data::Array{Float64, 1}, points::Array{Float64, 1}, bw::Float64, θ::Float64)
     d = Distributions.Normal(θ, bw)
-    y = Array{Float64}(length(points))
+    y = zeros(points)
+    #Compute point densities
     for (ii, xi) in enumerate(points)
         y[ii] = mean(Distributions.pdf(d, data .- xi))
     end
-    UnitKernelDensity(points, y, bw, length(points))
+    cdf = zeros(points)
+    #Compute approximate CDF
+    for ii in 2:length(points)
+        cdf[ii] = cdf[ii - 1] + 0.5 * (points[ii] - points[ii - 1]) * (y[ii] + y[ii - 1])
+    end
+    N = cdf[end]^(-1.0)
+    return UnitKernelDensity(points, N .* y, N .* cdf, bw, length(points))
+end
+
+"""
+Sample from Linearly Interpolated UnitKernelDensity
+"""
+function Distributions.sample(d::UnitKernelDensity)
+    r = rand()
+    k = searchsortedfirst(d.cdf, r)
+    return (r - d.cdf[k - 1]) / (d.cdf[k] - d.cdf[k - 1]) * (d.x[k] - d.x[k - 1]) + d.x[k - 1]
 end
 
 #rfft()
