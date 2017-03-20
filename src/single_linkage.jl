7d9dc1ee7d5117d180174d40343206cbfdf17c4d
Brendan McVeigh <mcveigh.brendan@gmail.com>
2017-03-19 17:53:24 -0700

    add functions for determining mode and proportions of single linkages by total number of links

--- src/single_linkage.jl
+++ src/single_linkage.jl
@@ -42,6 +42,27 @@ function logcount_single_linkage{G <: Integer}(nrow::G, ncol::G)
 end
 
 """
+Proportion of all single linkage matricies that 
+"""
+function logproportion_single_linkage{G <: Integer}(nrow::G, ncol::G)
+    if nrow < 1 || ncol < 1
+        error("nrow and ncol must both be postive integers")
+    end
+    maxLinks = min(nrow, ncol)
+    linkRatios = Array{Float64}(maxLinks)
+    for ii in 1:maxLinks
+        linkRatios[ii] = logratio_single_linkage(ii - 1, nrow, ncol)
+    end
+    logCounts = cumsum(linkRatios)
+    maxLogCounts = maximum(logCounts)
+    out = exp(-maxLogCounts)
+    for ct in logCounts
+        out += exp(ct - maxLogCounts)
+    end
+    return log(out) + maxLogCounts
+end
+
+"""
 Returns the number of possible single_linkage structures for `nlink` linkages and `nlink + 1` linkages, N(nlink + 1) / N(nlink)
 ```logratio_single_linkage(nlink, nrow, ncol)```
 """
@@ -55,7 +76,7 @@ Returns the number of possible single_linkage structures for `nlink1` linkages a
 """
 function logratio_single_linkage{G <: Integer}(nlink1::G, nlink2::G, nrow::G, ncol::G)
     if nlink1 == nlink2
-        return 0
+        return 0.0
     elseif nlink1 < nlink2
         out = 0.0
         nlink = nlink1
@@ -73,4 +94,15 @@ function logratio_single_linkage{G <: Integer}(nlink1::G, nlink2::G, nrow::G, nc
         end
         return out
     end
+end
+
+"""
+Find mode of single linkage structure, number of linkages that leads to the most possible linkages, incases where the mode is tied as with 15 rows and 20 columns the smaller is reported.
+"""
+function mode_single_linkage{G <: Integer}(nrow::G, ncol::G)
+    out = 0
+    while logratio_single_linkage(out, nrow, ncol) > 0.0
+        out += 1
+    end
+    return out
 end
