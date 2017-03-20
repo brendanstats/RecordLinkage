5fff00e7f19afc4a1de7e6671d358cf100498092
Brendan McVeigh <mcveigh.brendan@gmail.com>
2017-03-19 17:55:33 -0700

    constructor method from GridMatchMatrix moved to gridmatchmatrix.jl due to relying on GridMatchMatrix type

--- src/matchmatrix.jl
+++ src/matchmatrix.jl
@@ -11,8 +11,6 @@ end
 #Need to check
 MatchMatrix{G <: Integer}(nrow::G, ncol::G) = MatchMatrix(Array{G}(0), Array{G}(0), nrow, ncol)
 
-MatchMatrix{G <: Intger}(GM::GridMatchMatrix{G}) = MatchMatrix(getmatches(GM)..., GM.nrow, GM.ncol)
-
 """
 Check if supplied row and column correspond to a match in the matching matrix
 """
